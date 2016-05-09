ae061fca09e9a03b52174eb763e60f2e91891136
alittlefishy <mlin1508@gmail.com>
2016-05-08 21:15:38 -0400

    adding gender model

--- titanic/getting_started.py
+++ titanic/getting_started.py
@@ -2,7 +2,7 @@ import csv as csv
 import numpy as np
 
 # Open up the csv file in to a Python object
-csv_file_object = csv.reader(open('../csv/train.csv', 'rb'))
+csv_file_object = csv.reader(open('train.csv', 'rb'))
 header = csv_file_object.next()  # The next() command just skips the
                                  # first line which is a header
 data=[]                          # Create a variable called 'data'.
@@ -11,3 +11,54 @@ for row in csv_file_object:      # Run through each row in the csv file,
 data = np.array(data)            # Then convert from a list to an array
                                  # Be aware that each item is currently
                                  # a string in this format
+
+# The size() function counts how many elements are in
+# in the array and sum() (as you would expects) sums up
+# the elements in the array.
+
+number_passengers = np.size(data[0::,1].astype(np.float))
+number_survived = np.sum(data[0::,1].astype(np.float))
+proportion_survivors = number_survived / number_passengers
+
+women_only_stats = data[0::,4] == "female"
+men_only_stats = data[0::,4] != "female"
+
+# Using the index from above we select the females and males separately
+women_onboard = data[women_only_stats,1].astype(np.float)
+men_onboard = data[men_only_stats,1].astype(np.float)
+
+# Then we finds the proportions of them that survived
+proportion_women_survived = \
+                       np.sum(women_onboard) / np.size(women_onboard)
+proportion_men_survived = \
+                       np.sum(men_onboard) / np.size(men_onboard)
+
+# and then print it out
+print 'Proportion of women who survived is %s' % proportion_women_survived
+print 'Proportion of men who survived is %s' % proportion_men_survived
+
+test_file = open('test.csv', 'rb')
+test_file_object = csv.reader(test_file)
+header = test_file_object.next()
+
+prediction_file = open("genderbasedmodel.csv", "wb")
+prediction_file_object = csv.writer(prediction_file)
+
+prediction_file_object.writerow(["PassengerId", "Survived"])
+for row in test_file_object:       # For each row in test.csv
+    if row[3] == 'female':         # is it a female, if yes then
+        prediction_file_object.writerow([row[0],'1'])    # predict 1
+    else:                              # or else if male,
+        prediction_file_object.writerow([row[0],'0'])    # predict 0
+test_file.close()
+prediction_file.close()
+
+def main():
+    # print header
+    # print data[0]
+    # print data[-1]
+    # print data[0,3]
+    pass
+
+if __name__ == '__main__':
+    main()
